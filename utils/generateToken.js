34d3abe49624415827f79631bee67ffbf90564b6
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-27 20:31:33 +0200

    fix: generate JWT token properly as string

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const generateToken = async (id) => {
+const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
